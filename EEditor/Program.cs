ada767cfb8889fa63e12cf13ce18179bb5c0dfe0
capashaa <130944764+capashaa@users.noreply.github.com>
2025-08-26 16:31:46 +0200

    Added args

--- EEditor/Program.cs
+++ EEditor/Program.cs
@@ -11,11 +11,11 @@ namespace EEditor
         ///
 
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new MainForm(args));
         }
     }
 }
